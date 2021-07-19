595962c24c1a19732eaf9b4bbb77c6a7b0f46638
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-07-19 02:11:29 +0200

    add comments for explaining how to proceed for use/add/edit  providers

--- oauth-client/app/ExampleProvider.php
+++ oauth-client/app/ExampleProvider.php
@@ -4,10 +4,12 @@ use App\Provider;
 use App\Request;
 use App\ProviderInterface;
 
+// 2) You need to copy & paste this file for adding your provider, and juste changing the filename & the classname
 class ExampleProvider extends Provider implements ProviderInterface
 {
     private $code;
 
+    //In the bellow construc if you dont have options and header additional parameters let it to $options = [] & $headers = [] If you have an optional parameters you need to replace it juste by the variable name : $options, $headers
     public function __construct($client_id, $client_secret, $auth_url,  $access_token_url, $user_info_url, $redirect_uri, $options = [], $headers = [])
     {
         parent::__construct($client_id, $client_secret, $auth_url,  $access_token_url, $user_info_url, $redirect_uri, $options = [], $headers = []);
@@ -27,11 +29,15 @@ class ExampleProvider extends Provider implements ProviderInterface
     public function getUser($code)
     {
         $request = new Request();
+        //Here for google who needs specific headers parameters you need to add $this->headersparameters to the getAccessToken() method if you dont have headers specific parameters just use the same line without the $this->headersparameters parameter : $access_token = $this->getAccessToken($code, true);
         $access_token = $this->getAccessToken($code, true);
-        $headers = $request->setHeaders('GET', ["Authorization: Bearer ${access_token}", "User-Agent: github"]);
+
+        $headers = $request->setHeaders('GET', ["Authorization: Bearer ${access_token}", "User-Agent: github"]); // or  $access_token = ($code) ? $this->getAccessToken($code, true, $this->headers) : null; if header parameters
+
         return $access_token ? $request->get($this->user_info_url, $headers) : false;
     }
 
+    //Here you can edit the login link
     public function getAuthLink()
     {
         $link = $this->getAuthorizationUrl();
--- oauth-client/index.php
+++ oauth-client/index.php
@@ -1,4 +1,11 @@
 <?php
+
+/*
+ * 1) For adding a provider first you need to add it's configuration in the /app/config.php file
+ *
+ * 2) After that you need to copy one of the already existing Provider class and just changing the filename and the classname
+ */
+
 require __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 
 $configs = require('config.php');
@@ -10,15 +17,31 @@ $google = $collection->getProvider('google');
 $facebook = $collection->getProvider('facebook');
 $app = $collection->getProvider('app');
 
+/*
+ * 3) Finaly in this file you need to add a same line as above and edit the providerName called
+ *
+ * $exampleProvider = $collection->getProvider('example');
+ */
+
+
+// Here the call to the Provider->getAuthLink for displaying your link "Login with ..."
 echo $google->getAuthLink();
 echo $facebook->getAuthLink();
 echo $github->getAuthLink();
 echo $app->getAuthLink();
 
+/*
+ *  4) For adding the Login with link of your provider you need to add a same line as above and use the variable previously created and calling it's getAuthLink() method.
+ *
+ * echo $app->getAuthLink();
+ */
+
 ["code" => $code] = $_GET;
 
+// IF CODE IS NOT NULL -> call getUsers method of the provider and var_dump the user information given by the Oauth provider.
 if ($code){
 
+    //Dont display warning error of other method
     \error_reporting(0);
 
     if ($user = $google->getUser($code)){
@@ -40,5 +63,15 @@ if ($code){
         var_dump($user);
         die;
     }
+
+    /* 5)
+    *
+    * Last Step add the following if condition for displaying the user result when receiving the provider response
+    *
+    *    if ($user = $app->getTest($code)){
+    *        var_dump($user);
+    *        die;
+    *    }
+    */
 
 }
